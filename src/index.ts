2db295e15a1e42a6a25201ed55ea4f84543660f0
evaporator <osxar.jeavons@gmail.com>
2026-03-07 18:24:43 +0000

    added background generation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 const mainPage = safeFetch("mainpage");
 
-const genAPI: apiUrl = "https://evaporatoronline.org/generate"
-const solveApi = "https://evaporatoronline.org/generate"
+const baseApi = "https://evaporatoronline.org"
+const genAPI: apiUrl = `${baseApi}/generate`
+const solveApi: apiUrl = `${baseApi}/solve`
 
-type apiUrl = `http://${string}.${string}/${string}` | `https://${string}.${string}/${string}`
+type apiUrl = `http://${string}.${string}/${string}` | `https://${string}.${string}/${string}` | `http://localhost:8080/${string}`;
 
 getBackgroundMaze();
 
@@ -20,14 +21,29 @@ function safeFetch(elementName: string): HTMLElement{
 
 
 async function getBackgroundMaze(){
-    var Length = 40 * 16;
-    var Height = 40 * 9;
+    var multiplier = 8
+    var Length = multiplier * 16;
+    var Height = multiplier * 9;
     var Weight = 80;
     var Algorithm = "GrowingTree"
     var cacheFiller = Date.now();
+    var startXCoord = 0
+    var endXCoord = Length -1
+    var startYCoord = 0
+    var endYCoord = Height -1
+    var solveAlgorithm = "A*"
 
     const response = await fetch(`${genAPI}?Length=${Length}&Height=${Height}&Weight=${Weight}&Algorithm=${Algorithm}&_=${cacheFiller}`);
     const blob = await response.blob();
 
-    mainPage.style.backgroundImage = `url(${URL.createObjectURL(blob)}`;
+    const form = new FormData();
+    form.append("image", blob, "maze.png");
+    form.append("Start", `${startXCoord}-${startYCoord}`);
+    form.append("End", `${endXCoord}-${endYCoord}`);
+    form.append("Algorithm", solveAlgorithm);
+
+    const solveResponse = await fetch(`${solveApi}`, {method : "POST", body : form});
+    const solveBlob = await solveResponse.blob();
+
+    mainPage.style.backgroundImage = `url(${URL.createObjectURL(solveBlob)}`;
 }
